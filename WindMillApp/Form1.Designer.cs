eb87a55b5efd189f2236de707ed55dc0323b06a1
W9JAK <112979587+W9JAK@users.noreply.github.com>
2024-05-22 20:56:24 +0300

    Добавьте файлы проекта.

---
+++ WindMillApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindMillApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnChangeDirection = new System.Windows.Forms.Button();
+            this.pictureBoxMill = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlades = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlades)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(60, 66);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 75);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Запуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnChangeDirection
+            // 
+            this.btnChangeDirection.Location = new System.Drawing.Point(60, 147);
+            this.btnChangeDirection.Name = "btnChangeDirection";
+            this.btnChangeDirection.Size = new System.Drawing.Size(151, 75);
+            this.btnChangeDirection.TabIndex = 1;
+            this.btnChangeDirection.Text = "Сменить направление";
+            this.btnChangeDirection.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxMill
+            // 
+            this.pictureBoxMill.Location = new System.Drawing.Point(293, 33);
+            this.pictureBoxMill.Name = "pictureBoxMill";
+            this.pictureBoxMill.Size = new System.Drawing.Size(382, 372);
+            this.pictureBoxMill.TabIndex = 6;
+            this.pictureBoxMill.TabStop = false;
+            // 
+            // pictureBoxBlades
+            // 
+            this.pictureBoxBlades.Location = new System.Drawing.Point(280, 118);
+            this.pictureBoxBlades.Name = "pictureBoxBlades";
+            this.pictureBoxBlades.Size = new System.Drawing.Size(382, 318);
+            this.pictureBoxBlades.TabIndex = 5;
+            this.pictureBoxBlades.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(50, 264);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(183, 69);
+            this.trackBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 472);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBoxMill);
+            this.Controls.Add(this.pictureBoxBlades);
+            this.Controls.Add(this.btnChangeDirection);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlades)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnChangeDirection;
+        private System.Windows.Forms.PictureBox pictureBoxBlades;
+        private System.Windows.Forms.PictureBox pictureBoxMill;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ WindMillApp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindMillApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isRunning = false;
+        private bool direction = true;
+        private int speed = 5; // Начальная скорость теперь берется из начального значения trackBar1
+        private Timer timer1;
+        private float currentAngle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            try
+            {
+                pictureBoxMill.Image = Properties.Resources.WindMillWithoutBlades;
+                pictureBoxBlades.Image = Properties.Resources.WindmillBlades;
+                pictureBoxMill.BackColor = pictureBoxBlades.BackColor = Color.Transparent;
+                pictureBoxMill.SizeMode = pictureBoxBlades.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBoxBlades.Parent = pictureBoxMill;
+                pictureBoxBlades.Location = new Point(0, 0);
+
+                // Настройка TrackBar
+                trackBar1.Minimum = 3;
+                trackBar1.Maximum = 17;
+                trackBar1.Value = 10;
+                trackBar1.TickFrequency = 1;
+                trackBar1.ValueChanged += TrackBar1_ValueChanged; // Подписка на событие изменения значения
+
+                // Настройка таймера
+                timer1 = new Timer { Interval = 100 };
+                timer1.Tick += timer1_Tick;
+
+                btnStart.Click += btnStart_Click;
+                btnChangeDirection.Click += btnChangeDirection_Click;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            speed = trackBar1.Value; // Обновление скорости в соответствии с положением ползунка
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            isRunning = !isRunning;
+            btnStart.Text = isRunning ? "Остановить" : "Запустить";
+            timer1.Enabled = isRunning;
+        }
+
+        private void btnChangeDirection_Click(object sender, EventArgs e)
+        {
+            direction = !direction;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentAngle += (direction ? speed : -speed);
+            pictureBoxBlades.Image = RotateImage(Properties.Resources.WindmillBlades, currentAngle);
+        }
+
+        private Bitmap RotateImage(Image image, float angle)
+        {
+            Bitmap bmp = new Bitmap(image.Width, image.Height);
+            bmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Transparent);
+                g.TranslateTransform(image.Width / 2f, image.Height / 2f);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-image.Width / 2f, -image.Height / 2f);
+                g.DrawImage(image, new Point(0, 0));
+            }
+            return bmp;
+        }
+    }
+}
